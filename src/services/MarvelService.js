521b848d6961b967fd867d1dbd37ed284cfcb8e9
GrimdvlLaptop <grimdvl@gmail.com>
2024-08-25 18:28:08 +0300

    lesson 6 done

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -33,7 +33,8 @@ class MarvelService {
             description: descr === '' ? descr = "There's no information about this character." : descr,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
